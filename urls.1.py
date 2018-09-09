bd01e54461355f4016fe41f7142af5677bb315ea
EC2 Default User <ec2-user@ip-172-31-46-24.ap-southeast-1.compute.internal>
2018-09-09 11:33:40 +0000

    commit

--- urls.1.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-from django.contrib.auth.views import login, logout
-
-urlpatterns = [
-    url(r'^$', views.table_list, name='table_list'),
-    url(r'^confirm/(?P<select_tableId>[0-9]+)/$', views.confirm, name='confirm'),
-    url(r'^reservation/(?P<select_tableId>[0-9]+)/$', views.reservation, name='reservation')
-]
