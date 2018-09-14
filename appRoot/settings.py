079c1f6066b8dc0868d42cb6b434b51b08d671cd
Toshiki Tanaka <toshiki.tanaka@gmail.com>
2018-09-14 13:29:32 +0000

    commit

--- appRoot/settings.py
+++ appRoot/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'appRoot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'FOOD_COURT_APP_DB',
-#        'HOST': 'dbinstance-groupf.cnrlgiui7hgv.ap-northeast-1.rds.amazonaws.com',
-#        'USER': 'admin',
-#        'PASSWORD': 'password',
-        'OPTIONS': {
-            'read_default_file': './my.cnf',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'NAME': 'FOOD_COURT_APP_DB',
+        'HOST': 'dbinstance-groupf.cnrlgiui7hgv.ap-northeast-1.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+#        'OPTIONS': {
+#            'read_default_file': './my.cnf',
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#        }
     }
 }
 
