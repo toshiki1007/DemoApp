6fd5da814bd8142fc9f5d70ac96e85d5d11d23dd
toshiki <toshiki1007@gmail.com>
2018-09-24 19:08:37 +0000

    commit

--- food_court_app/consts.py
+++ food_court_app/consts.py
@@ -7,14 +7,14 @@ ORDER_URL = HOME_URL + "order_page/"
 S3 = "s3"
 
 #本番用
-#S3_PATH = "https://s3-ap-northeast-1.amazonaws.com/"\
-#    +"ec2-52-194-11-29.ap-northeast-1.compute.amazonaws.com/"
-#S3_BUCKET_NAME = "ec2-52-194-11-29.ap-northeast-1.compute.amazonaws.com"    
+S3_PATH = "https://s3-ap-northeast-1.amazonaws.com/"\
+    +"ec2-52-194-11-29.ap-northeast-1.compute.amazonaws.com/"
+S3_BUCKET_NAME = "ec2-52-194-11-29.ap-northeast-1.compute.amazonaws.com"    
 
 #開発環境用
-S3_PATH = "https://s3-ap-southeast-1.amazonaws.com/"\
-    +"groupf-test-bucket/"
-S3_BUCKET_NAME = "groupf-test-bucket"    
+#S3_PATH = "https://s3-ap-southeast-1.amazonaws.com/"\
+#    +"groupf-test-bucket/"
+#S3_BUCKET_NAME = "groupf-test-bucket"    
 
 PNG = ".png"
 UNDER_BAR = "_"
